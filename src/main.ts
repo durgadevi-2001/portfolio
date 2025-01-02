d7e0ed82a332245fd2b087f0b0396fba61844577
durgadevi-2001 <durgadevi.atwork@gmail.com>
2025-01-02 18:49:22 +0530

    issue fix

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-// import { provideRouter } from '@angular/router';
+import { provideRouter, withDebugTracing } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-
-import { provideRouter, withDebugTracing } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes, withDebugTracing())],
 }).catch((err) => console.error(err));
+
